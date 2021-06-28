fa48f8d1be8d642772f6bf18acecd74540282ae4
Darkcat <andrewlichao@gmail.com>
2021-06-28 20:55:58 +0000

    v0.02

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
+document.addEventListener("scroll", function(){
+  var scroll = document.documentElement.scrollTop
+  var largestBoard = 1.5
+  var smallestBoard = 1
+  var mostBlur = 15
+  var leastBlur = 0
+  var scrollStart = 0
+  var scrollStop = 300
+  if(scroll < 300){
+    console.log((scroll * (largestBoard-smallestBoard)/(scrollStop-scrollStart)) + smallestBoard)
+    console.log(document.getElementById("corkboard").style)
+    document.getElementById("corkboard").style.transform = "scale("+((scroll * (largestBoard-smallestBoard)/(scrollStop-scrollStart)) + smallestBoard)+","+((scroll * (largestBoard-smallestBoard)/(scrollStop-scrollStart)) + smallestBoard)+")"
 
+    document.getElementById("corkboard").style.filter = "blur("+(mostBlur - (scroll * (mostBlur-leastBlur)/(scrollStop-scrollStart)))+"px)"
+  } else {
+    document.getElementById("corkboard").style.transform = "scale("+largestBoard+","+largestBoard+")"
+    document.getElementById("corkboard").style.filter = "blur(0px)"
+  }
+});
 
+// 70 -> 100.  1 -> 1.5.   15 -> 0
+// 0 -> 300.   0 -> 300
+
+// (scroll * (1.5-1)/(300-0)) + 1
